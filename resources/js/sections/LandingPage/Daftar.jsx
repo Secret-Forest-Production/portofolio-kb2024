3f914ef55b6332137d7fcaecac0550b6d972e822
anantariskys <gamersindo555@gmail.com>
2024-08-16 23:29:30 +0700

    Update Daftar.jsx

--- resources/js/sections/LandingPage/Daftar.jsx
+++ resources/js/sections/LandingPage/Daftar.jsx
@@ -20,12 +20,12 @@ const Daftar = () => {
         stiffness:300
       }}
       src={TitleHeader} draggable='false' alt="Kami Menantikan Partisipasimu" className='mx-auto' />
-      <p className='font-jakarta text-2xl text-center  font-light text-primary500'>Jangan lewatkan lomba-lomba dan forum daerah</p>
+      <p className='font-jakarta  lg:text-2xl text-center  font-light text-primary500'>Jangan lewatkan lomba-lomba dan forum daerah</p>
       <img src={Dekorasi} draggable='false' alt="dekorasi" className='mx-auto' />
       <motion.button 
       whileTap={{scale:0.95}}
       
-      className='px-8 py-2 rounded-3xl hover:bg-primary300 duration-300 ease-in-out bg-primary500 text-white font-jakarta text-xl '>Daftar Sekarang</motion.button>
+      className='px-8 py-2 rounded-3xl hover:bg-primary300 duration-300 ease-in-out bg-primary500 text-white font-jakarta lg:text-xl '>Daftar Sekarang</motion.button>
     </section>
   )
 }
