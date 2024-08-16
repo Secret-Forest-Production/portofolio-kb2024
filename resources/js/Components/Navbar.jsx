4d299fca1adb70b022282a0c3a2e4dc5e6903464
anantariskys <gamersindo555@gmail.com>
2024-08-16 23:06:15 +0700

    Feat : add responsive navbar

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import LogoImage from '../../assets/logotext.png';
+import { Icon } from '@iconify/react';
+
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,27 +20,51 @@ const Navbar = () => {
 
   const navItems = ['Beranda', 'Galeri', 'FAQ'];
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className={`w-full fixed top-0 z-50 font-poppins duration-300 ease-in-out  text-primary300 bg-transparent ${isScrolled ? 'bg-opacity-50  backdrop-blur-sm' : ' '}`}>
-      <main className='flex container mx-auto justify-between'>
-        <img src={LogoImage} draggable='false' alt="logo" className='w-52 rounded-b-md py-2' />
-        <ul className='lg:flex gap-10 hidden  items-center text-lg font-medium'>
+    <nav className={`w-full fixed top-0 z-50 font-poppins duration-300 ease-in-out text-primary300 bg-transparent ${isScrolled ? 'bg-opacity-50 backdrop-blur-sm' : ''}`}>
+      <main className='flex container mx-auto justify-between items-center py-2'>
+        <img src={LogoImage} draggable='false' alt="logo" className='lg:w-52 w-32 rounded-b-md' />
+
+        {/* Mobile Menu Button */}
+        <button className="lg:hidden text-3xl text-primary500" onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? <Icon icon="mdi:close" /> : <Icon icon="mdi:menu" />}
+        </button>
+
+        {/* Desktop Navigation */}
+        <ul className='lg:flex gap-10 hidden items-center text-lg font-medium'>
           {navItems.map((item, index) => (
             <NavItem key={index}>{item}</NavItem>
           ))}
-          <button className={`text-white bg-primary500 hover:bg-primary300 duration-300 ease-in-out hover:scale-95 px-7 py-1 rounded-md `}>
+          <button className="text-white bg-primary500 hover:bg-primary300 duration-300 ease-in-out hover:scale-95 px-7 py-1 rounded-md">
+            Daftar
+          </button>
+        </ul>
+
+        {/* Mobile Navigation */}
+        <ul className={`lg:hidden fixed top-0 left-0 h-screen py-4 w-3/4 px-2 bg-primary500 text-white text-lg font-medium transform duration-300 ease-in-out ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        <img src={LogoImage} className='w-32 ' alt=""  />
+          {navItems.map((item, index) => (
+            <NavItem key={index} className="lg:py-4 px-4 py-2 border-b lg:text-base text-sm border-primary300">
+              {item}
+            </NavItem>
+          ))}
+          <button className="w-full text-primary500 bg-white hover:bg-primary300 duration-300 ease-in-out hover:scale-95 px-7 py-2 text-sm  rounded-md mt-4">
             Daftar
           </button>
         </ul>
       </main>
     </nav>
   );
-}
+};
 
-const NavItem = ({ children }) => {
+const NavItem = ({ children, className }) => {
   return (
-    <li>{children}</li>
+    <li className={className}>{children}</li>
   );
-}
+};
 
 export default Navbar;
