3509cdebeb1adb131c9cc7512d9ba4751d5e794f
anantariskys <gamersindo555@gmail.com>
2024-08-17 10:48:49 +0700

    Update Medpar.jsx

--- resources/js/sections/LandingPage/Medpar.jsx
+++ resources/js/sections/LandingPage/Medpar.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import DekorAtas from "../../../assets/dekormedparatas.png";
 import DekorBawah from "../../../assets/dekormedparbawah.png";
-import IconCloud from "@/Components/magicui/icon-cloud";
-
-import Logo from "../../../assets/logo.png";
 import Marquee from "@/Components/magicui/marquee";
+import MedparData from "@/Data/Medpar";
 
 const Medpar = () => {
-    const slugs = [Logo, Logo, Logo, Logo, Logo];
     return (
         <section className="py-10 space-y-4  font-jakarta">
             <div className="flex container mx-auto flex-col gap-4 items-center">
                 <img src={DekorAtas} alt="dekoratas" draggable="false" />
-                <h1 className="font-extrabold text-3xl text-center lg:text-7xl text-primary300">
+                <h1 className="font-extrabold text-3xl text-center lg:text-6xl text-primary300">
                     OUR SPONSOR
                 </h1>
             </div>
-            <Marquee pauseOnHover className="[--duration:20s] ">
-                {slugs.map((item, i) => (
-                    <img src={item} key={i} className="w-32" alt="" />
+            <Marquee pauseOnHover className="[--duration:40s] ">
+                {MedparData.map((item, i) => (
+                    <img src={item} key={i} className="w-32 aspect-square object-contain" alt="" />
                 ))}
             </Marquee>
-            <Marquee pauseOnHover reverse className="[--duration:20s] ">
-                {slugs.map((item, i) => (
-                    <img src={item} key={i} className="w-32" alt="" />
+            <Marquee pauseOnHover reverse className="[--duration:40s] ">
+                {MedparData.map((item, i) => (
+                    <img src={item} key={i} className="w-32 aspect-square object-contain" alt="" />
                 ))}
             </Marquee>
 
             <div className="flex flex-col container mx-auto gap-4 items-center">
-                <h1 className="font-extrabold text-3xl text-center lg:text-7xl text-primary300">
+                <h1 className="font-extrabold text-3xl text-center lg:text-6xl text-primary300">
                     AND MEDIA PARTNER
                 </h1>
                 <img src={DekorBawah} alt="dekorbawah" draggable="false" />
