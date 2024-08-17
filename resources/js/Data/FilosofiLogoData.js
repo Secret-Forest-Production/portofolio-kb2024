8e2138f1bea2b1e2cfda6cdaa9838b7d01b888b3
anantariskys <gamersindo555@gmail.com>
2024-08-17 10:49:06 +0700

    Create FilosofiLogoData.js

---
+++ resources/js/Data/FilosofiLogoData.js
@@ -0,0 +1,45 @@
+import Logo1 from "../../assets/FilosofiLogo/logo_01.webp";
+import Logo2 from "../../assets/FilosofiLogo/Logo_02.webp";
+import Logo3 from "../../assets/FilosofiLogo/Logo_03.webp";
+import Logo4 from "../../assets/FilosofiLogo/Logo_04.webp";
+import Logo5 from "../../assets/FilosofiLogo/Logo_05.webp";
+
+const FilosofiLogoData = [
+    {
+        id: 0,
+        logoDescription: "PESUT MAHAKAM MELAMBANGKAN PENJAGA WARISAN",
+        detailDescription:
+            "memiliki makna pentingnya peran generasi muda dalam melestarikan dan memadukan adat budaya daerah Indonesia di era modernisasi",
+        logoImage: Logo1,
+    },
+    {
+        id: 1,
+        logoDescription: "WARNA PADA PESUT MAHAKAM",
+        detailDescription:
+            "Hijau melambangkan kehidupan dan pertumbuhan, Biru melambangkan kemakmuran dan keseimbangan",
+        logoImage: Logo2,
+    },
+    {
+        id: 2,
+        logoDescription: "3 STRIP PADA PESUT MAHAKAM",
+        detailDescription:
+            "bermakna Tri Dharma Perguruan Tinggi yakni pendidikan dan pengajaran, penelitian, pengabdian kepada masyarakat.",
+        logoImage: Logo3,
+    },
+    {
+        id: 3,
+        logoDescription: "5 NEGATIVE SPACE PADA BADAN PESUT MAHAKAM",
+        detailDescription:
+            "Menandakan 5 simbolis Sila sebagai identitas negara Indonesia",
+        logoImage: Logo4,
+    },
+    {
+        id: 4,
+        logoDescription: "BENTUK AIR PADA EKOR PESUT MAHAKAM",
+        detailDescription:
+            "Memiliki arti Keterhubungan dan Ketergantungan. Bermakna yang menekankan setiap elemen - elemen untuk kolaborasi dan kerja sama.",
+        logoImage: Logo5,
+    },
+];
+
+export default FilosofiLogoData;
