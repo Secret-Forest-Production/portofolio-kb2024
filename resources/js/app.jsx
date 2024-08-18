6834e2964490c5e34d9612ac2883e025cf3735c5
anantariskys <gamersindo555@gmail.com>
2024-08-18 12:26:57 +0700

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,28 +1,29 @@
-import './bootstrap';
-import '../css/app.css';
-
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import AppLayout from './Layouts/AppLayout';
+import './bootstrap';
+import '../css/app.css';
 
 const appName = import.meta.env.VITE_APP_NAME;
 
 createInertiaApp({
-    title: (title) => `${appName}||${title}`,
+    title: (title) => `${appName} || ${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx'))
-    .then((page) => {
-        if (page.default.layout === undefined) {
-            page.default.layout = (page) => page;
-        }
-        return page;
-    }),      setup({ el, App, props }) {
-        if (import.meta.env.DEV) {
-            createRoot(el).render(<App {...props} />);
-            return
-        }
-
-        hydrateRoot(el, <App {...props} />);
+        .then((page) => {
+            if (page.default.layout === undefined) {
+                page.default.layout = (page) => page;
+            }
+            return page;
+        }),
+    setup({ el, App, props }) {
+        ReactDOM.createRoot(el).render(
+            <BrowserRouter>
+                <App {...props} />
+            </BrowserRouter>
+        );
     },
     progress: {
         color: '#4B5563',
