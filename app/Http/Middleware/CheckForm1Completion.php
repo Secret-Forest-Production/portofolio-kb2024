915b6113ba8f8c5ef2c1b9f4085756ff3ffdfdd3
MUEHEHEHEH <143472197+MUEHEHEHEH@users.noreply.github.com>
2024-09-04 22:59:11 +0700

    fix : Daftar route handling

---
+++ app/Http/Middleware/CheckForm1Completion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckForm1Completion
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $id = $request->route('jenisLomba');
+
+        if (!session('isStep1Complete')) {
+            // Redirect ke form1 jika belum selesai
+            return Inertia::location(route('daftar1', ['jenisLomba' => $id]));
+        }
+
+        return $next($request);
+    }
+}
